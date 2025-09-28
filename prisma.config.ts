d570c3520ef30f1a3fa8181ca751cbf38f529e4e
pranav-suri-phc <pranavsuri.dev@projecthumancity.com>
2025-09-28 17:23:29 +0530

    added zod generator to prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import { defineConfig } from "prisma/config";
-import "dotenv/config";
+import "dotenv/config"; // Loads .env file contents into process.env and is used by index.prisma
 
 export default defineConfig({
   schema: path.join("prisma/schema"),
